d98b23be41271e8ad47431596b734ffdf9c44bde
sattwik <maitisattwik@gmail.com>
2022-12-18 08:40:10 +0530

    g

--- src/components/Feed/Feed.js
+++ src/components/Feed/Feed.js
@@ -1,14 +1,26 @@
 import React from 'react'
 import './Feed.css'
+import SimpleImageSlider from "react-simple-image-slider";
 import p1 from './1.png'
 import p2 from './2.png'
 import p3 from './8.png'
 import p4 from './5.png'
 import p5 from './1.png'
 import p6 from './1.png'
+import { faAlignCenter } from '@fortawesome/free-solid-svg-icons';
 
 
 const Feed = () => {
+  const images = [
+    { url: p1 },
+    { url:p2 },
+    { url: p3},
+    { url: p4 },
+    { url: p3},
+    { url: p2},
+    { url: p1 },
+  ];
+  
   return (
     <>
     <div className="container my-4">
@@ -19,7 +31,7 @@ const Feed = () => {
           </div>
         </div>
         <div className="col">
-        <div class="containerr">
+        <div className="containerr">
   <img src={p2} alt="Avatar" className="image"/>
   <div className="middle">
     <div className ="text">France</div>
@@ -36,7 +48,7 @@ const Feed = () => {
        </div>
        <div className="row">
        <div className="col">
-        <div class="containerr">
+        <div className="containerr">
   <img src={p4} alt="Avatar" className="image"/>
   <div className="middle">
     <div className ="text">Australia</div>
@@ -55,7 +67,7 @@ const Feed = () => {
              
         </div>
         <div className="col">
-        <div class="containerr">
+        <div className="containerr">
   <img src={p3} alt="Avatar" className="image"/>
   <div className="middle">
     <div className ="text">Maldives</div>
@@ -63,6 +75,28 @@ const Feed = () => {
 </div>
 </div>
        </div>
+       <br />
+       <br />
+        <h1 style={{color:'white',textAlign:'center'}}>Property Delux Rooms </h1>
+        <div className="imageslider">
+        <div>
+      <SimpleImageSlider
+          className="lol"
+        width={600}
+        height={400}
+        images={images}
+        showBullets={true}
+        showNavs={true}
+        style={{boxShadow: 'rgba(0, 0, 0, 0.56) 0px 22px 70px 4px'}}
+        autoPlay={true}
+        autoPlayDelay={2.0}
+        slideDuration={1}
+      />
+    </div>
+
+
+
+        </div>
         
           
     
