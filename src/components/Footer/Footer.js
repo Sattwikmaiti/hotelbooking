6618036085b44b756c5eefa3f35217fbc594ffce
sattwik <maitisattwik@gmail.com>
2022-12-18 11:58:19 +0530

    g

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,9 +2,23 @@ import React from 'react'
 import './Footer.css'
 import {faPaperPlane} from  "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import GoogleIcon from '@mui/icons-material/Google';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import NavigationIcon from '@mui/icons-material/Navigation';
 const Footer = () => {
   return (
+    <>
+    <div className="top">
+    <h1>Subscribe to our Daily Newsletter for more offers and timely discount coupons @&#x20B9;999 yearly only!!!</h1>
+        <br />
+        <br></br>
+        </div>
     <div className="foot">
+    <div className="my-4 ">
+        
        <form action="#">
                                     <div class="form-row">
                                         <div class="col dk-footer-form">
@@ -15,7 +29,31 @@ const Footer = () => {
                                         </div>
                                     </div>
                                 </form>
-    </div>
+                                
+                                </div>
+                                
+        </div>
+
+        <div className="connect" style={{backgroundColor:'grey',padding :'20px',display:'flex',justifyContent:'center'}}>
+            
+              
+              <div className="ic" style={{padding:'20px'}}>
+                <h4 style={{display:'flex',justifyContent:'center'}}>  Follow  Us</h4>
+              <GoogleIcon className="opl" />
+              <TwitterIcon className="opl"/>
+              <GitHubIcon className="opl"/>
+              <LinkedInIcon className="opl"/>
+              <FacebookIcon className="opl"/>
+                </div>
+
+                
+        </div>
+                   <a href="#"><div className="my-5" style={{display:'flex',justifyContent:'center'}}><NavigationIcon /></div> </a>            
+
+
+                                
+    
+    </>
   )
 }
 
